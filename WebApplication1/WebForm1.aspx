bbe7ca020d60cdc116795487a212d42815d9b506
alka_ra <alyarog@gmail.com>
2017-03-27 22:50:57 +0300

    first commit

---
+++ WebApplication1/WebForm1.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="WebForm1.aspx.cs" Inherits="WebApplication1.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div style="font-size: xx-large">
+    
+        Currency Converter</div>
+        from&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:TextBox ID="TextBox1" runat="server" style="margin-top: 8px"></asp:TextBox>
+&nbsp;&nbsp;
+        <asp:DropDownList ID="DropDownList1" runat="server" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged">
+            <asp:ListItem>BYN</asp:ListItem>
+            <asp:ListItem>USD</asp:ListItem>
+            <asp:ListItem>EUR</asp:ListItem>
+            <asp:ListItem>RUB</asp:ListItem>
+            <asp:ListItem>UAH</asp:ListItem>
+        </asp:DropDownList>
+&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; to&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        &nbsp;&nbsp;<asp:DropDownList ID="DropDownList2" runat="server" OnSelectedIndexChanged="DropDownList2_SelectedIndexChanged">
+            <asp:ListItem>BYN</asp:ListItem>
+            <asp:ListItem>USD</asp:ListItem>
+            <asp:ListItem>EUR</asp:ListItem>
+            <asp:ListItem>RUB</asp:ListItem>
+            <asp:ListItem>UAH</asp:ListItem>
+        </asp:DropDownList>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <asp:Button ID="Button1" runat="server" Font-Size="Medium" Text="Convert" OnClick="Button1_Click" />
+        <br />
+        <br />
+        <asp:TextBox ID="TextBox2" runat="server" Height="67px" Width="220px"></asp:TextBox>
+        <br />
+    </form>
+</body>
+</html>
---
+++ WebApplication1/WebForm1.aspx.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication1
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        static Object[,] array = new Object[6,6] {
+                                    { "Currency", "BYN",  "USD",  "EUR",  "RUB",  "UAH"},
+                                    { "BYN",           1, 0.530,  0.492, 30.627,  14.294},
+                                    { "USD",      1.885,      1,  0.927, 57.744,  26.950},
+                                    { "EUR",      2.034,  1.079,      1,  62.292, 29.073},
+                                    { "RUB",      0.033,  0.017,  0.016,      1,  0.467},
+                                    { "UAH",      0.07,   0.037,  0.034,  2.143,      1},
+                                };
+        
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+           
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void DropDownList2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            double a;
+            string b, c;
+            a = Convert.ToDouble(TextBox1.Text);
+            b = DropDownList1.Text;
+            c = DropDownList2.Text;
+
+            int ii = 0;
+            int jj = 0;
+            for(int i = 1; i < array.GetLength(1); i++)
+            {
+                if (b.Equals((String) array[0,i]))
+                {
+                    ii = i;
+                }
+                if (c.Equals((String)array[0, i]))
+                {
+                    jj = i;
+                }
+            }
+
+            double result;
+            result = a * Convert.ToDouble(array[ii, jj]);
+
+            TextBox2.Text = Convert.ToString(result);
+
+        }
+
+    }
+}
